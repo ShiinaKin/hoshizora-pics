02812869864319e7defcd0a22f07e4584bf1052d
Shiina Kin <shiina@sakurasou.io>
2024-09-11 13:14:43 +0800

    refactor: rm ui test code

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -1,23 +1,9 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from "vue-router";
-import type { ApiImageIdGetRequest, DefaultApi } from "api-client";
-
-const apiImageIdGetRequest: ApiImageIdGetRequest = {
-  id: 1
-};
-
-const apiClient = new DefaultApi();
-
-function fetchData() {
-  apiClient.apiImageIdGet(apiImageIdGetRequest).then((response) => {
-    console.log(response.data);
-  });
-}
 </script>
 
 <template>
   <RouterView />
-  <button @click="fetchData">Send</button>
 </template>
 
 <style scoped></style>
