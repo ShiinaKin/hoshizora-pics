8f8040d94e721fc6beb6a963fbe0311f659a5ff0
Shiina Kin <shiina@sakurasou.io>
2024-09-14 16:32:35 +0800

    fix: systemStatus cannot be serialized

--- app/src/main/kotlin/io/sakurasou/model/setting/SystemStatus.kt
+++ app/src/main/kotlin/io/sakurasou/model/setting/SystemStatus.kt
@@ -1,9 +1,12 @@
 package io.sakurasou.model.setting
 
+import kotlinx.serialization.Serializable
+
 /**
  * @author Shiina Kin
  * 2024/9/13 15:58
  */
+@Serializable
 data class SystemStatus(
     val isInit: Boolean
 ) : SettingConfig()
