27c07901862c67a7165db2f2352c7a7cd2b3e078
Shiina Kin <shiina@sakurasou.io>
2024-10-13 22:11:23 +0800

    feat(ImageController): implement image upload, update, fetch, and delete routes
    Add support for handling self-managed and admin-managed image
    operations. Enhance functionalities to handle file uploads, fetch
    image files, and respond with either file bytes or redirect URLs.
    Improve validation and error handling for image uploads.

--- app/src/main/kotlin/io/sakurasou/controller/ImageController.kt
+++ app/src/main/kotlin/io/sakurasou/controller/ImageController.kt
@@ -5,17 +5,23 @@ import io.ktor.http.*
 import io.ktor.http.content.*
 import io.ktor.server.application.*
 import io.ktor.server.request.*
+import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import io.sakurasou.constant.*
+import io.sakurasou.controller.request.ImageManagePatchRequest
 import io.sakurasou.controller.request.ImagePatchRequest
-import io.sakurasou.controller.vo.CommonResponse
-import io.sakurasou.controller.vo.ImagePageVO
-import io.sakurasou.controller.vo.ImageVO
-import io.sakurasou.controller.vo.PageResult
+import io.sakurasou.controller.request.ImageRawFile
+import io.sakurasou.controller.request.PageRequest
+import io.sakurasou.controller.vo.*
+import io.sakurasou.exception.controller.param.UnsupportedFileTypeException
+import io.sakurasou.exception.controller.param.WrongParameterException
+import io.sakurasou.extension.getPrincipal
 import io.sakurasou.extension.pageRequest
+import io.sakurasou.extension.success
+import io.sakurasou.model.dto.ImageFileDTO
 import io.sakurasou.plugins.AuthorizationPlugin
 import io.sakurasou.service.image.ImageService
-import java.io.ByteArrayOutputStream
+import io.swagger.v3.oas.models.media.Schema
 
 /**
  * @author ShiinaKin
@@ -45,9 +51,11 @@ private fun Route.imageSelfRoute(controller: ImageController) {
     }) {
         imageSelfDelete(controller)
         imageSelfUpdate(controller)
-        imageSelfFetch(controller)
-        imageSelfPage(controller)
+        imageSelfInfoFetch(controller)
+        imageSelfFileFetch(controller)
+        imageSelfThumbnailFileFetch(controller)
     }
+    imageSelfPage(controller)
 }
 
 private fun Route.imageSelfUpload(controller: ImageController) {
@@ -64,39 +72,55 @@ private fun Route.imageSelfUpload(controller: ImageController) {
             }
             response {
                 HttpStatusCode.OK to {
-                    description = "success"
-                    body<CommonResponse<Unit>> { }
+                    description = "success, return image external id, or empty string if it is private"
+                    body<CommonResponse<String>> { }
                 }
             }
         })
         {
-            val contentLength = call.request.header(HttpHeaders.ContentLength)?.toIntOrNull() ?: 0
+            val userId = call.getPrincipal().id
 
-
-            var fileDescription = ""
-            var fileName = ""
             val multipartData = call.receiveMultipart()
 
-            val fileBytes = ByteArrayOutputStream(contentLength).apply {
-                multipartData.forEachPart { part ->
-                    when (part) {
-                        is PartData.FormItem -> {
-                            fileDescription = part.value
-                        }
+            var imageRawFile: ImageRawFile? = null
 
-                        is PartData.FileItem -> {
-                            fileName = part.originalFileName as String
-                            writeBytes(part.streamProvider().readBytes())
+            var isMoreThanOne = -1
+            multipartData.forEachPart { part ->
+                when (part) {
+                    is PartData.FileItem -> {
+                        if (isMoreThanOne >= 0) {
+                            throw WrongParameterException("Only one file is allowed")
                         }
+                        if (part.contentType?.match(ContentType.Image.Any) == false) {
+                            throw UnsupportedFileTypeException()
+                        }
+                        val fileName = part.originalFileName as String
+                        val mimeType = part.contentType!!.toString()
+                        val bytes = part.streamProvider().readBytes()
+                        val contentLength = bytes.size.toLong()
 
-                        else -> {}
+                        imageRawFile = ImageRawFile(
+                            name = fileName,
+                            mimeType = mimeType,
+                            size = contentLength,
+                            bytes = bytes
+                        )
+
+                        isMoreThanOne++
                     }
-                    part.dispose()
+
+                    else -> {}
                 }
-            }.toByteArray()
+                part.dispose()
+            }
 
-            TODO()
+            if (isMoreThanOne < 0 || imageRawFile == null) {
+                throw WrongParameterException("No file found")
+            }
 
+            val imageName = controller.handleSelfUpload(userId, imageRawFile!!)
+
+            call.success(imageName)
         }
     }
 }
@@ -114,7 +138,10 @@ private fun Route.imageSelfDelete(controller: ImageController) {
                 }
             }
         }) {
-            TODO()
+            val userId = call.getPrincipal().id
+            val imageId = call.imageId()
+            controller.handleSelfDelete(userId, imageId)
+            call.success()
         }
     }
 }
@@ -137,12 +164,37 @@ private fun Route.imageSelfUpdate(controller: ImageController) {
                 }
             }
         }) {
-            TODO()
+            val userId = call.getPrincipal().id
+            val imageId = call.imageId()
+            val imagePatchRequest = call.receive<ImagePatchRequest>()
+            controller.handleSelfUpdate(userId, imageId, imagePatchRequest)
+            call.success()
         }
     }
 }
 
-private fun Route.imageSelfFetch(controller: ImageController) {
+private fun Route.imageSelfInfoFetch(controller: ImageController) {
+    route {
+        install(AuthorizationPlugin) {
+            permission = IMAGE_READ_SELF_SINGLE
+        }
+        get("info", {
+            response {
+                HttpStatusCode.OK to {
+                    description = "success"
+                    body<CommonResponse<ImageVO>> { }
+                }
+            }
+        }) {
+            val userId = call.getPrincipal().id
+            val imageId = call.imageId()
+            val imageVO = controller.handleSelfFetch(userId, imageId)
+            call.success(imageVO)
+        }
+    }
+}
+
+private fun Route.imageSelfFileFetch(controller: ImageController) {
     route {
         install(AuthorizationPlugin) {
             permission = IMAGE_READ_SELF_SINGLE
@@ -151,11 +203,63 @@ private fun Route.imageSelfFetch(controller: ImageController) {
             response {
                 HttpStatusCode.OK to {
                     description = "success"
-                    body<CommonResponse<ImageVO>> { }
+                    body(Schema<Any>().apply {
+                        oneOf(
+                            listOf(
+                                Schema<String>().apply {
+                                    type = "string"
+                                    description = "S3"
+                                },
+                                Schema<ByteArray>().apply {
+                                    type = "byteArray"
+                                    description = "Local"
+                                }
+                            )
+                        )
+                    })
                 }
             }
         }) {
-            TODO()
+            val userId = call.getPrincipal().id
+            val imageId = call.imageId()
+            val imageFileDTO = controller.handleSelfFileFetch(userId, imageId)
+            if (imageFileDTO.bytes != null) call.respondBytes(imageFileDTO.bytes, ContentType.Image.Any)
+            else call.respondRedirect(imageFileDTO.url!!)
+        }
+    }
+}
+
+private fun Route.imageSelfThumbnailFileFetch(controller: ImageController) {
+    route {
+        install(AuthorizationPlugin) {
+            permission = IMAGE_READ_SELF_SINGLE
+        }
+        get("thumbnail", {
+            response {
+                HttpStatusCode.OK to {
+                    description = "success"
+                    body(Schema<Any>().apply {
+                        oneOf(
+                            listOf(
+                                Schema<String>().apply {
+                                    type = "string"
+                                    description = "S3"
+                                },
+                                Schema<ByteArray>().apply {
+                                    type = "byteArray"
+                                    description = "Local"
+                                }
+                            )
+                        )
+                    })
+                }
+            }
+        }) {
+            val userId = call.getPrincipal().id
+            val imageId = call.imageId()
+            val imageFileDTO = controller.handleSelfThumbnailFileFetch(userId, imageId)
+            if (imageFileDTO.bytes != null) call.respondBytes(imageFileDTO.bytes, ContentType.Image.Any)
+            else call.respondRedirect(imageFileDTO.url!!)
         }
     }
 }
@@ -179,7 +283,10 @@ private fun Route.imageSelfPage(controller: ImageController) {
                 }
             }
         }) {
-            TODO()
+            val userId = call.getPrincipal().id
+            val pageRequest = call.pageRequest()
+            val pageResult = controller.handleSelfPage(userId, pageRequest)
+            call.success(pageResult)
         }
     }
 }
@@ -187,12 +294,6 @@ private fun Route.imageSelfPage(controller: ImageController) {
 private fun Route.imageManageRoute(controller: ImageController) {
     route("manage", {
         protected = true
-        request {
-            pathParameter<Long>("id") {
-                description = "image id"
-                required = true
-            }
-        }
         response {
             HttpStatusCode.NotFound to {
                 description = "image not found"
@@ -210,9 +311,11 @@ private fun Route.imageManageRoute(controller: ImageController) {
         }) {
             imageManageDelete(controller)
             imageManageUpdate(controller)
-            imageManageFetch(controller)
-            imageManagePage(controller)
+            imageManageInfoFetch(controller)
+            imageManageFileFetch(controller)
+            imageManageThumbnailFileFetch(controller)
         }
+        imageManagePage(controller)
     }
 }
 
@@ -229,7 +332,9 @@ private fun Route.imageManageDelete(controller: ImageController) {
                 }
             }
         }) {
-            TODO()
+            val imageId = call.imageId()
+            controller.handleManageDelete(imageId)
+            call.success()
         }
     }
 }
@@ -241,7 +346,7 @@ private fun Route.imageManageUpdate(controller: ImageController) {
         }
         patch({
             request {
-                body<ImagePatchRequest> {
+                body<ImageManagePatchRequest> {
                     required = true
                 }
             }
@@ -252,31 +357,98 @@ private fun Route.imageManageUpdate(controller: ImageController) {
                 }
             }
         }) {
-            TODO()
+            val imageId = call.imageId()
+            val imagePatchRequest = call.receive<ImageManagePatchRequest>()
+            controller.handleManageUpdate(imageId, imagePatchRequest)
+            call.success()
         }
     }
 }
 
-private fun Route.imageManageFetch(controller: ImageController) {
+private fun Route.imageManageInfoFetch(controller: ImageController) {
+    route {
+        install(AuthorizationPlugin) {
+            permission = IMAGE_READ_ALL_SINGLE
+        }
+        get("info", {
+            response {
+                HttpStatusCode.OK to {
+                    description = "success"
+                    body<CommonResponse<ImageManageVO>> { }
+                }
+            }
+        }) {
+            val imageId = call.imageId()
+            val imageManageVO = controller.handleManageFetch(imageId)
+            call.success(imageManageVO)
+        }
+    }
+}
+
+private fun Route.imageManageFileFetch(controller: ImageController) {
     route {
         install(AuthorizationPlugin) {
             permission = IMAGE_READ_ALL_SINGLE
         }
         get({
-            request {
-                pathParameter<Long>("imageId") {
-                    description = "image id"
-                    required = true
-                }
-            }
             response {
                 HttpStatusCode.OK to {
                     description = "success"
-                    body<CommonResponse<ImageVO>> { }
+                    body(Schema<Any>().apply {
+                        oneOf(
+                            listOf(
+                                Schema<String>().apply {
+                                    type = "string"
+                                    description = "S3"
+                                },
+                                Schema<ByteArray>().apply {
+                                    type = "byteArray"
+                                    description = "Local"
+                                }
+                            )
+                        )
+                    })
                 }
             }
         }) {
-            TODO()
+            val imageId = call.imageId()
+            val imageFileDTO = controller.handleManageFileFetch(imageId)
+            if (imageFileDTO.bytes != null) call.respondBytes(imageFileDTO.bytes, ContentType.Image.Any)
+            else call.respondRedirect(imageFileDTO.url!!)
+        }
+    }
+}
+
+private fun Route.imageManageThumbnailFileFetch(controller: ImageController) {
+    route {
+        install(AuthorizationPlugin) {
+            permission = IMAGE_READ_ALL_SINGLE
+        }
+        get("thumbnail", {
+            response {
+                HttpStatusCode.OK to {
+                    description = "success"
+                    body(Schema<Any>().apply {
+                        oneOf(
+                            listOf(
+                                Schema<String>().apply {
+                                    type = "string"
+                                    description = "S3"
+                                },
+                                Schema<ByteArray>().apply {
+                                    type = "byteArray"
+                                    description = "Local"
+                                }
+                            )
+                        )
+                    })
+                }
+            }
+        }) {
+            val imageId = call.imageId()
+            val imageFileDTO = controller.handleManageThumbnailFileFetch(imageId)
+            if (imageFileDTO.bytes != null) call.respondBytes(imageFileDTO.bytes, ContentType.Image.Any)
+            else call.respondRedirect(imageFileDTO.url!!)
         }
     }
 }
@@ -291,7 +463,7 @@ private fun Route.imageManagePage(controller: ImageController) {
             response {
                 HttpStatusCode.OK to {
                     description = "success"
-                    body<PageResult<ImagePageVO>> {
+                    body<CommonResponse<PageResult<ImagePageVO>>> {
                         description = "page result"
                     }
                 }
@@ -300,12 +472,77 @@ private fun Route.imageManagePage(controller: ImageController) {
                 }
             }
         }) {
-            TODO()
+            val pageRequest = call.pageRequest()
+            val pageResult = controller.handleManagePage(pageRequest)
+            call.success(pageResult)
         }
     }
 }
 
+private fun ApplicationCall.imageId() =
+    parameters["imageId"]?.toLongOrNull() ?: throw IllegalArgumentException("imageId")
+
 class ImageController(
     private val imageService: ImageService
 ) {
+    suspend fun handleSelfUpload(userId: Long, imageRawFile: ImageRawFile): String {
+        val imageName = imageService.saveImage(userId, imageRawFile)
+        return imageName
+    }
+
+    suspend fun handleSelfDelete(userId: Long, imageId: Long) {
+        imageService.deleteSelfImage(userId, imageId)
+    }
+
+    suspend fun handleSelfUpdate(userId: Long, imageId: Long, imagePatchRequest: ImagePatchRequest) {
+        imageService.patchSelfImage(userId, imageId, imagePatchRequest)
+    }
+
+    suspend fun handleSelfFetch(userId: Long, imageId: Long): ImageVO {
+        val imageVO = imageService.fetchSelfImageInfo(userId, imageId)
+        return imageVO
+    }
+
+    suspend fun handleSelfFileFetch(userId: Long, imageId: Long): ImageFileDTO {
+        val imageFileDTO = imageService.fetchSelfImageFile(userId, imageId)
+        return imageFileDTO
+    }
+
+    suspend fun handleSelfThumbnailFileFetch(userId: Long, imageId: Long): ImageFileDTO {
+        val imageFileDTO = imageService.fetchSelfImageThumbnailFile(userId, imageId)
+        return imageFileDTO
+    }
+
+    suspend fun handleSelfPage(userId: Long, pageRequest: PageRequest): PageResult<ImagePageVO> {
+        val pageResult = imageService.pageSelfImage(userId, pageRequest)
+        return pageResult
+    }
+
+    suspend fun handleManageDelete(imageId: Long) {
+        imageService.deleteImage(imageId)
+    }
+
+    suspend fun handleManageUpdate(imageId: Long, imagePatchRequest: ImageManagePatchRequest) {
+        imageService.patchImage(imageId, imagePatchRequest)
+    }
+
+    suspend fun handleManageFetch(imageId: Long): ImageManageVO {
+        val imageManageVO = imageService.fetchImageInfo(imageId)
+        return imageManageVO
+    }
+
+    suspend fun handleManageFileFetch(imageId: Long): ImageFileDTO {
+        val imageFileDTO = imageService.fetchImageFile(imageId)
+        return imageFileDTO
+    }
+
+    suspend fun handleManageThumbnailFileFetch(imageId: Long): ImageFileDTO {
+        val imageFileDTO = imageService.fetchImageThumbnailFile(imageId)
+        return imageFileDTO
+    }
+
+    suspend fun handleManagePage(pageRequest: PageRequest): PageResult<ImageManagePageVO> {
+        val pageResult = imageService.pageImage(pageRequest)
+        return pageResult
+    }
 }
