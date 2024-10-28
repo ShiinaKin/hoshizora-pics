73d8e8858fc58e00ba2fd53dbb8cd65558b990ae
Shiina Kin <shiina@sakurasou.io>
2024-10-28 19:46:57 +0800

    feat(ui): add useUserFieldStore

--- ui/src/stores/counter.ts
+++ ui/src/stores/counter.ts
@@ -19,4 +19,27 @@ export const useCommonStore = defineStore("common", () => {
 
   return { title, setTitle, subTitle, setSubTitle, allowSignup, setAllowSignup };
 });
+
+export const useUserFieldStore = defineStore("userField", () => {
+  const userId = ref(-1);
+  const setUserId = (newUserId: number) => {
+    userId.value = newUserId;
+  };
+
+  const username = ref("");
+  const setUsername = (newUsername: string) => {
+    username.value = newUsername;
+  };
+
+  const email = ref("");
+  const setEmail = (newEmail: string) => {
+    email.value = newEmail;
+  };
+
+  const groupName = ref("");
+  const setGroupName = (newGroupName: string) => {
+    groupName.value = newGroupName;
+  };
+
+  return { userId, setUserId, username, setUsername, email, setEmail, groupName, setGroupName };
 });
