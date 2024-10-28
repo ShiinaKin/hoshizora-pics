7cdd09c71d722288747b68c2e453d2797a0ed262
Shiina Kin <shiina@sakurasou.io>
2024-10-28 19:28:43 +0800

    feat(ui): add siteInit route

--- ui/src/router/index.ts
+++ ui/src/router/index.ts
@@ -4,6 +4,17 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
+      path: "/",
+      name: "home",
+      children: [
+        {
+          path: "init",
+          name: "siteInit",
+          component: () => import("@/views/init/SiteInitView.vue")
+        },
+      ],
+    },
+    {
       path: "/:pathMatch(.*)",
       name: "notFound",
       component: () => import("@/views/error/NotFoundView.vue")
