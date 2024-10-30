d1c8c809f98658b8ca65e9cdb9e8f5ee6afc887c
Shiina Kin <shiina@sakurasou.io>
2024-10-30 23:06:50 +0800

    refactor(App.vue): move commonSiteSetting logic from router to App.vue for centralized management and streamline navigation logic
    refactor(router/index.ts): remove commonRouteGuard logic to simplify routing and avoid redundancy by moving site setting checks to App.vue

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -1,5 +1,33 @@
 <script setup lang="ts">
-import { RouterView } from "vue-router";
+import { RouterView, useRoute, useRouter } from "vue-router";
+import { useCommonStore } from "@/stores/counter";
+import { CommonApi } from "api-client";
+
+const route = useRoute();
+const router = useRouter();
+const commonStore = useCommonStore();
+const commonApi = new CommonApi();
+
+const token = localStorage.getItem("token");
+
+commonApi
+  .apiSiteSettingGet()
+  .then((response) => {
+    const resp = response.data;
+    if (resp.isSuccessful) {
+      const commonSiteSetting = resp.data!!;
+      commonStore.setTitle(commonSiteSetting.siteTitle!);
+      commonStore.setSubTitle(commonSiteSetting.siteSubTitle!);
+      commonStore.setAllowSignup(commonSiteSetting.siteAllowSignup!);
+
+      if (!commonSiteSetting.isSiteInit!!) router.push({ name: "siteInit" });
+      if (!token) router.push({ name: "login" });
+      if (route.name === "siteInit") router.push({ name: "overview" });
+    }
+  })
+  .catch((error) => {
+    console.error(error);
+  });
 </script>
 
 <template>
--- ui/src/router/index.ts
+++ ui/src/router/index.ts
@@ -1,9 +1,4 @@
-import {
-  createRouter,
-  createWebHistory,
-  type RouteLocationNormalizedGeneric,
-  type RouteLocationNormalizedLoadedGeneric
-} from "vue-router";
+import { createRouter, createWebHistory } from "vue-router";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -27,8 +22,6 @@ const router = createRouter({
           name: "register",
           component: () => import("@/views/auth/RegisterView.vue")
         }
-      ],
-      beforeEnter: (to, from) => commonRouteGuard(to, from)
     },
     {
       path: "/:pathMatch(.*)",
@@ -40,30 +33,5 @@ const router = createRouter({
 
 export default router;
 
-import { useCommonStore } from "@/stores/counter";
-import { CommonApi } from "api-client";
 
-const commonRouteGuard = async (to: RouteLocationNormalizedGeneric, from: RouteLocationNormalizedLoadedGeneric) => {
-  if (to.name === from.name) return;
-
-  if (to.name === "home" || (from.name === "siteInit" && to.name === "login")) {
-    const commonApi = new CommonApi();
-    const commonStore = useCommonStore();
-
-    return commonApi
-      .apiSiteSettingGet()
-      .then((response) => {
-        if (response.isSuccessful) {
-          const commonSiteSetting = response.data!!;
-          commonStore.setTitle(commonSiteSetting.siteTitle!);
-          commonStore.setSubTitle(commonSiteSetting.siteSubTitle!);
-          commonStore.setAllowSignup(commonSiteSetting.siteAllowSignup!);
-          if (commonSiteSetting.isSiteInit!!) return { name: "login" };
-          else return { name: "siteInit" };
-        }
-      })
-      .catch((error) => {
-        console.error(error);
-      });
   }
-};
