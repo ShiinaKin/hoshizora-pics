59755b16b0e9264ba5d26e3405e56d791c4b26ec
Shiina Kin <shiina@sakurasou.io>
2024-10-31 14:02:24 +0800

    feat(App.vue): dynamically update document title and meta description

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import { onMounted } from "vue";
 import { RouterView, useRoute, useRouter } from "vue-router";
 import { useCommonStore } from "@/stores/counter";
 import { CommonApi } from "api-client";
@@ -10,24 +11,33 @@ const commonApi = new CommonApi();
 
 const token = localStorage.getItem("token");
 
-commonApi
-  .apiSiteSettingGet()
-  .then((response) => {
-    const resp = response.data;
-    if (resp.isSuccessful) {
-      const commonSiteSetting = resp.data!!;
-      commonStore.setTitle(commonSiteSetting.siteTitle!);
-      commonStore.setSubTitle(commonSiteSetting.siteSubTitle!);
-      commonStore.setAllowSignup(commonSiteSetting.siteAllowSignup!);
+onMounted(() => {
+  commonApi
+    .apiSiteSettingGet()
+    .then((response) => {
+      const resp = response.data;
+      if (resp.isSuccessful) {
+        const commonSiteSetting = resp.data!;
+        commonStore.setTitle(commonSiteSetting.siteTitle!);
+        commonStore.setSubTitle(commonSiteSetting.siteSubTitle!);
+        commonStore.setSiteDescription(commonSiteSetting.siteDescription!);
+        commonStore.setAllowSignup(commonSiteSetting.siteAllowSignup!);
 
-      if (!commonSiteSetting.isSiteInit!!) router.push({ name: "siteInit" });
-      if (!token) router.push({ name: "login" });
-      if (route.name === "siteInit") router.push({ name: "overview" });
-    }
-  })
-  .catch((error) => {
-    console.error(error);
-  });
+        document.title = commonStore.title + " - " + commonStore.subTitle;
+        const metaDescription = document.querySelector('meta[name="description"]');
+        if (metaDescription) {
+          metaDescription.setAttribute("content", commonStore.subTitle);
+        }
+
+        if (!commonSiteSetting.isSiteInit!!) router.push({ name: "siteInit" });
+        if (!token) router.push({ name: "login" });
+        if (route.name === "siteInit") router.push({ name: "overview" });
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+});
 </script>
 
 <template>
