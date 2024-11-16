c724de9618e5c4b06d59d6482f45f56bc2ae3866
Shiina Kin <shiina@sakurasou.io>
2024-11-16 17:47:57 +0800

    fix: correct handling of front-end routing

--- app/src/main/kotlin/io/sakurasou/plugins/Routing.kt
+++ app/src/main/kotlin/io/sakurasou/plugins/Routing.kt
@@ -1,12 +1,16 @@
 package io.sakurasou.plugins
 
 import io.github.smiley4.ktorswaggerui.dsl.routing.route
+import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.http.content.*
 import io.ktor.server.plugins.autohead.*
 import io.ktor.server.plugins.statuspages.*
+import io.ktor.server.request.*
 import io.ktor.server.resources.*
+import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import io.ktor.utils.io.*
 import io.sakurasou.config.apiRoute
 import io.sakurasou.config.exceptionHandler
 import io.sakurasou.controller.commonRoute
@@ -14,7 +18,16 @@ import io.sakurasou.di.InstanceCenter.commonService
 
 fun Application.configureRouting() {
     install(Resources)
-    install(StatusPages) { exceptionHandler() }
+    install(StatusPages) {
+        status(HttpStatusCode.NotFound) { call, _ ->
+            if (call.request.uri.startsWith("/api")
+                || call.request.uri.startsWith("/s")
+                || call.request.uri.startsWith("/random")
+            ) call.respond(HttpStatusCode.NotFound)
+            else call.respondSource(call.resolveResource("index.html", "static")!!.readFrom().readRemaining())
+        }
+        exceptionHandler()
+    }
     install(AutoHeadResponse)
     routing {
         apiRoute()
