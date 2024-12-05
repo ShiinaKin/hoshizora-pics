b6a4a9900693b4833a1f6054fc0455fb71c5d89b
Shiina Kin <shiina@sakurasou.io>
2024-12-05 14:13:31 +0800

    refactor(i18n): restructure user profile view localization keys

--- ui/src/views/userField/user/UserProfileView.vue
+++ ui/src/views/userField/user/UserProfileView.vue
@@ -2,23 +2,17 @@
 import { ref } from "vue";
 import { useUserFieldStore } from "@/stores/counter";
 import { useI18n } from "vue-i18n";
-import {
-  Configuration,
-  UserApi,
-  type UserVO,
-} from "api-client";
+import { Configuration, UserApi, type UserVO } from "api-client";
 import { useToast } from "primevue/usetoast";
-import { useField, useForm } from "vee-validate";
-import { toTypedSchema } from "@vee-validate/yup";
+import { useForm } from "vee-validate";
 import * as yup from "yup";
-import ToggleSwitch from "primevue/toggleswitch";
+import VeeIftaInputText from "@/components/vee-input/VeeIftaInputText.vue";
+import VeeToggleSwitch from "@/components/vee-input/VeeToggleSwitch.vue";
 import Divider from "primevue/divider";
 import Button from "primevue/button";
 import { Icon } from "@iconify/vue";
 import Dialog from "primevue/dialog";
-import IftaLabel from "primevue/iftalabel";
-import InputText from "primevue/inputtext";
-import Toolbar from 'primevue/toolbar';
+import Toolbar from "primevue/toolbar";
 import { formatUTCStringToLocale } from "@/utils/DateTimeUtils";
 
 const userFieldStore = useUserFieldStore();
@@ -32,54 +26,48 @@ const userApi = new UserApi(configuration);
 
 const userVO = ref<UserVO>();
 
-const passwordRegExp = RegExp("^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,32}$");
-const userEditValidationSchema = toTypedSchema(
-  yup
-    .object({
-      patchEmail: yup.string().trim()
-        .email(t("myProfileView.profileEditFormEmailValidationFailedMessage"))
-        .nullable(),
-      patchPassword: yup
-        .string()
-        .trim()
-        .test("password", t("myProfileView.profileEditFormPasswordValidationFailedMessage"), (value) => {
-          return (value === undefined || value === "" || passwordRegExp.test(value));
-        }),
-      patchIsDefaultImagePrivate: yup.boolean()
-    })
-    .test("at-least-one-field", t("myProfileView.profileEditFormValidationFailedMessage"), (values) => {
-      return (
-        values.patchEmail !== undefined && values.patchEmail !== "" ||
-        values.patchPassword !== undefined && passwordRegExp.test(values.patchPassword) ||
-        values.patchIsDefaultImagePrivate !== undefined
+const userEditValidationSchema = yup.object({
+  email: yup
+    .string()
+    .trim()
+    .email(t("myProfileView.edit.dialog.form.verify.email.invalid"))
+    .test("at-least-one-field", t("myProfileView.edit.dialog.form.verify.atLeastOneField"), function () {
+      return !!(
+        this.parent.email ||
+        this.parent.password ||
+        (this.parent.isDefaultImagePrivate !== undefined && this.parent.isDefaultImagePrivate !== userVO.value?.isDefaultImagePrivate)
+      );
+    }),
+  password: yup
+    .string()
+    .trim()
+    .min(8, t("myProfileView.edit.dialog.form.verify.password.min"))
+    .max(32, t("myProfileView.edit.dialog.form.verify.password.max"))
+    .matches(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]+$/, t("myProfileView.edit.dialog.form.verify.password.invalid"))
+    .test("at-least-one-field", t("myProfileView.edit.dialog.form.verify.atLeastOneField"), function () {
+      return !!(
+        this.parent.email ||
+        this.parent.password ||
+        (this.parent.isDefaultImagePrivate !== undefined && this.parent.isDefaultImagePrivate !== userVO.value?.isDefaultImagePrivate)
+      );
+    }),
+  isDefaultImagePrivate: yup
+    .boolean()
+    .test("at-least-one-field", t("myProfileView.edit.dialog.form.verify.atLeastOneField"), function () {
+      return !!(
+        this.parent.email ||
+        this.parent.password ||
+        (this.parent.isDefaultImagePrivate !== undefined && this.parent.isDefaultImagePrivate !== userVO.value?.isDefaultImagePrivate)
       );
     })
-);
-
-const { handleSubmit, meta, errors, validate } = useForm({
-  validationSchema: userEditValidationSchema,
-  validateOnMount: false
 });
-
-const { value: patchEmail, handleBlur: emailBlur } = useField<string | undefined>("patchEmail");
-const { value: patchPassword, handleBlur: passwordBlur } = useField<string | undefined>("patchPassword");
-const { value: patchIsDefaultImagePrivate } = useField<boolean | undefined>("patchIsDefaultImagePrivate");
-
-const handleBlur = (field: "email" | "password") => {
-  if (field === "email") {
-    emailBlur();
-  } else {
-    passwordBlur();
-  }
-  validate();
-};
+const { handleSubmit } = useForm({
+  validationSchema: userEditValidationSchema
+});
 
 const profileEditDialog = ref(false);
 
 function showProfileEditDialog() {
-  patchEmail.value = undefined;
-  patchPassword.value = undefined;
-  patchIsDefaultImagePrivate.value = undefined;
   profileEditDialog.value = true;
 }
 
@@ -93,6 +81,13 @@ function fetchUser() {
       if (resp.isSuccessful) {
         userVO.value = resp.data!;
         userFieldStore.setEmail(userVO.value.email!);
+      } else {
+        toast.add({
+          severity: "success",
+          summary: t("myProfileView.detail.toast.failedTitle"),
+          detail: resp.message,
+          life: 3000
+        });
       }
     })
     .catch((error) => {
@@ -106,18 +101,28 @@ const editUser = handleSubmit((values) => {
   userApi
     .apiUserSelfPatch({
       userSelfPatchRequest: {
-        email: values.patchEmail === "" ? undefined : values.patchEmail,
-        password: values.patchPassword === "" ? undefined : values.patchPassword,
-        isDefaultImagePrivate: values.patchIsDefaultImagePrivate
+        email: values.email === "" ? undefined : values.email,
+        password: values.password === "" ? undefined : values.password,
+        isDefaultImagePrivate: values.isDefaultImagePrivate
       }
     })
     .then((response) => {
       const resp = response.data;
       if (resp.isSuccessful) {
         fetchUser();
-        toast.add({ severity: "success", summary: "Success", detail: resp.message, life: 3000 });
+        toast.add({
+          severity: "success",
+          summary: t("myProfileView.edit.toast.successTitle"),
+          detail: resp.message,
+          life: 3000
+        });
       } else {
-        toast.add({ severity: "error", summary: "Error", detail: resp.message, life: 3000 });
+        toast.add({
+          severity: "error",
+          summary: t("myProfileView.edit.toast.failedTitle"),
+          detail: resp.message,
+          life: 3000
+        });
       }
     })
     .catch((error) => {
@@ -131,7 +136,7 @@ const editUser = handleSubmit((values) => {
   <div class="w-full flex flex-col gap-4 bg-gray-100 p-8">
     <Toolbar class="rounded-2xl border-none text-sm px-6">
       <template #start>
-        <h2 class="text-lg">{{ t("myProfileView.profileTitle") }}</h2>
+        <h2 class="text-lg">{{ t("myProfileView.toolbar.title") }}</h2>
       </template>
       <template #end>
         <button
@@ -139,7 +144,7 @@ const editUser = handleSubmit((values) => {
           @click="showProfileEditDialog"
         >
           <Icon icon="mdi:account-edit-outline" />
-          {{ t("myProfileView.profileUpdateButton") }}
+          {{ t("myProfileView.toolbar.updateButton") }}
         </button>
       </template>
     </Toolbar>
@@ -148,78 +153,82 @@ const editUser = handleSubmit((values) => {
       <div class="flow-root">
         <dl class="-my-3 divide-y divide-gray-100 text-sm">
           <div class="grid grid-cols-1 gap-1 py-3 sm:grid-cols-3 sm:gap-4">
-            {{ t("myProfileView.profileUserId") }}
+            {{ t("myProfileView.detail.userId") }}
             <dd class="text-gray-700 sm:col-span-2">{{ userVO?.id }}</dd>
           </div>
 
           <div class="grid grid-cols-1 gap-1 py-3 sm:grid-cols-3 sm:gap-4">
-            <dt class="font-medium text-gray-900">{{ t("myProfileView.profileUsername") }}</dt>
+            <dt class="font-medium text-gray-900">{{ t("myProfileView.detail.username") }}</dt>
             <dd class="text-gray-700 sm:col-span-2">{{ userVO?.username }}</dd>
           </div>
 
           <div class="grid grid-cols-1 gap-1 py-3 sm:grid-cols-3 sm:gap-4">
-            <dt class="font-medium text-gray-900">{{ t("myProfileView.profileEmail") }}</dt>
+            <dt class="font-medium text-gray-900">{{ t("myProfileView.detail.email") }}</dt>
             <dd class="text-gray-700 sm:col-span-2">
               {{ userVO?.email }}
             </dd>
           </div>
 
           <div class="grid grid-cols-1 gap-1 py-3 sm:grid-cols-3 sm:gap-4">
-            <dt class="font-medium text-gray-900">{{ t("myProfileView.profileCreateTime") }}</dt>
+            <dt class="font-medium text-gray-900">{{ t("myProfileView.detail.createTime") }}</dt>
             <dd class="text-gray-700 sm:col-span-2">
               {{ formatUTCStringToLocale(userVO?.createTime) }}
             </dd>
           </div>
 
           <div class="grid grid-cols-1 gap-1 py-3 sm:grid-cols-3 sm:gap-4">
-            <dt class="font-medium text-gray-900">{{ t("myProfileView.profileBanStatus") }}</dt>
+            <dt class="font-medium text-gray-900">{{ t("myProfileView.detail.banStatus.title") }}</dt>
             <dd class="text-gray-700 sm:col-span-2">
-              {{ userVO?.isBanned ? t("myProfileView.profileBanStatusBanned") : t("myProfileView.profileBanStatusNormal") }}
+              {{
+                userVO?.isBanned
+                  ? t("myProfileView.detail.banStatus.banned")
+                  : t("myProfileView.detail.banStatus.normal")
+              }}
             </dd>
           </div>
 
           <div class="grid grid-cols-1 gap-1 py-3 sm:grid-cols-3 sm:gap-4">
-            <dt class="font-medium text-gray-900">{{ t("myProfileView.profileUserGroupName") }}</dt>
+            <dt class="font-medium text-gray-900">{{ t("myProfileView.detail.groupName") }}</dt>
             <dd class="text-gray-700 sm:col-span-2">{{ userVO?.groupName }}</dd>
           </div>
 
           <div class="grid grid-cols-1 gap-1 py-3 sm:grid-cols-3 sm:gap-4">
-            <dt class="font-medium text-gray-900">{{ t("myProfileView.profileImageCount") }}</dt>
+            <dt class="font-medium text-gray-900">{{ t("myProfileView.detail.imageCount") }}</dt>
             <dd class="text-gray-700 sm:col-span-2">
-              {{ userVO?.imageCount }} {{ t("myProfileView.profileImageCountUnit") }}
+              {{ userVO?.imageCount }} {{ t("myProfileView.detail.imageCountUnit") }}
             </dd>
           </div>
 
           <div class="grid grid-cols-1 gap-1 py-3 sm:grid-cols-3 sm:gap-4">
-            <dt class="font-medium text-gray-900">{{ t("myProfileView.profileAlbumCount") }}</dt>
+            <dt class="font-medium text-gray-900">{{ t("myProfileView.detail.albumCount") }}</dt>
             <dd class="text-gray-700 sm:col-span-2">
-              {{ userVO?.albumCount }} {{ t("myProfileView.profileAlbumCountUnit") }}
+              {{ userVO?.albumCount }} {{ t("myProfileView.detail.albumCountUnit") }}
             </dd>
           </div>
 
           <div class="grid grid-cols-1 gap-1 py-3 sm:grid-cols-3 sm:gap-4">
-            <dt class="font-medium text-gray-900">{{ t("myProfileView.profileUsedSpace") }}</dt>
+            <dt class="font-medium text-gray-900">{{ t("myProfileView.detail.usedSpace") }}</dt>
             <dd class="text-gray-700 sm:col-span-2">
               {{ userVO?.totalImageSize.toFixed(2) }}
-              {{ t("myProfileView.profileSpaceSizeUnit") }}
+              {{ t("myProfileView.detail.spaceSizeUnit") }}
             </dd>
           </div>
 
           <div class="grid grid-cols-1 gap-1 py-3 sm:grid-cols-3 sm:gap-4">
-            <dt class="font-medium text-gray-900">{{ t("myProfileView.profileUsableSpace") }}</dt>
+            <dt class="font-medium text-gray-900">{{ t("myProfileView.detail.usableSpace") }}</dt>
             <dd class="text-gray-700 sm:col-span-2">
               {{ userVO?.allSize.toFixed(2) }}
-              {{ t("myProfileView.profileSpaceSizeUnit") }}
+              {{ t("myProfileView.detail.spaceSizeUnit") }}
             </dd>
           </div>
 
           <div class="grid grid-cols-1 gap-1 py-3 sm:grid-cols-3 sm:gap-4">
-            <dt class="font-medium text-gray-900">{{ t("myProfileView.profileIsDefaultImagePrivate") }}</dt>
+            <dt class="font-medium text-gray-900">{{ t("myProfileView.detail.isDefaultImagePrivate.title") }}</dt>
             <dd class="text-gray-700 sm:col-span-2">
               {{
                 userVO?.isDefaultImagePrivate
-                  ? t("myProfileView.profileIsDefaultImagePrivateTrue")
-                  : t("myProfileView.profileIsDefaultImagePrivateFalse")
+                  ? t("myProfileView.detail.isDefaultImagePrivate.true")
+                  : t("myProfileView.detail.isDefaultImagePrivate.false")
               }}
             </dd>
           </div>
@@ -228,57 +237,43 @@ const editUser = handleSubmit((values) => {
       <Divider />
     </div>
 
-    <Dialog v-model:visible="profileEditDialog" modal :header="t('myProfileView.profileEditDialogTitle')">
+    <Dialog v-model:visible="profileEditDialog" modal :header="t('myProfileView.edit.dialog.title')">
       <form @submit="editUser">
         <div class="flex flex-col gap-4 m-4 w-96">
           <div class="flex flex-col gap-4 w-full">
-            <IftaLabel variant="on">
-              <InputText
+            <div class="flex flex-col gap-1">
+              <VeeIftaInputText
                 id="editUserEmail"
-                v-model="patchEmail"
-                :placeholder="userVO!.email!"
-                class="w-full"
-                @blur="handleBlur('email')"
+                name="email"
+                :placeholder="userVO?.email!"
+                :label="t('myProfileView.edit.dialog.form.email')"
               />
-              <label for="editUserEmail">{{ t("myProfileView.profileEditFormEmail") }}</label>
-              <p class="text-red-500 text-sm">{{ errors.patchEmail }}</p>
-            </IftaLabel>
-
-            <IftaLabel variant="on">
-              <InputText
+            </div>
+            <div class="flex flex-col gap-1">
+              <VeeIftaInputText
                 id="editUserPassword"
-                v-model="patchPassword"
-                :placeholder="t('myProfileView.profileEditFormPasswordPlaceholder')"
-                class="w-full"
+                name="password"
                 type="password"
-                @blur="handleBlur('password')"
+                :label="t('myProfileView.edit.dialog.form.password')"
               />
-              <label for="editUserPassword">{{ t("myProfileView.profileEditFormPasswordPlaceholder") }}</label>
-              <p class="text-red-500 text-sm">{{ errors.patchPassword }}</p>
-            </IftaLabel>
-
-            <div>
-              <div class="w-full flex items-center px-2 gap-4">
-                <label for="editDefaultImageVisible" class="text-sm"
-                  >{{ t("myProfileView.profileEditFormIsDefaultImagePrivate") }}:
-                </label>
-                <ToggleSwitch
-                  id="editDefaultImageVisible"
-                  v-model="patchIsDefaultImagePrivate"
-                  :default-value="userVO?.isDefaultImagePrivate"
-                />
-              </div>
-              <p class="text-red-500 text-sm">{{ errors.patchIsDefaultImagePrivate }}</p>
+            </div>
+            <div class="flex flex-col gap-1">
+              <VeeToggleSwitch
+                id="editDefaultImageVisible"
+                name="isDefaultImagePrivate"
+                :default-value="userVO?.isDefaultImagePrivate!"
+                :label="t('myProfileView.edit.dialog.form.isDefaultImagePrivate')"
+              />
             </div>
           </div>
           <div class="flex justify-end gap-2">
             <Button
               type="button"
-              :label="t('myProfileView.profileEditFormCancelButton')"
+              :label="t('myProfileView.edit.dialog.form.cancelButton')"
               severity="secondary"
               @click="profileEditDialog = false"
             />
-            <Button type="submit" :label="t('myProfileView.profileEditFormSubmitButton')" :disabled="!meta.valid" />
+            <Button type="submit" :label="t('myProfileView.edit.dialog.form.submitButton')" />
           </div>
         </div>
       </form>
