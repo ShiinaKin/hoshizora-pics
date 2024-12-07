d59974e0c7f6d97b56e53d0991e55b84862465c0
Shiina Kin <shiina@sakurasou.io>
2024-12-07 23:29:59 +0800

    refactor(ui): refactor siteInitView, improve form handling and validation
    - Replace manual input handling with vee-validate and yup for form validation
    - Update components to use VeeFloatInputText for consistent input styling
    - Add validation schema to ensure input complies with required rules

--- ui/src/views/init/SiteInitView.vue
+++ ui/src/views/init/SiteInitView.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
-import { ref } from "vue";
 import { useRouter } from "vue-router";
 import { useI18n } from "vue-i18n";
-import { CommonApi, type SiteInitRequest } from "api-client";
-import { Icon } from "@iconify/vue";
 import { useToast } from "primevue/usetoast";
+import { useForm } from "vee-validate";
+import * as yup from "yup";
+import { CommonApi, type SiteInitRequest } from "api-client";
+import VeeFloatInputText from "@/components/vee-input/VeeFloatInputText.vue";
+import Button from "primevue/button";
 
 const { t } = useI18n();
 const router = useRouter();
@@ -12,24 +14,65 @@ const toast = useToast();
 
 const commonApi = new CommonApi();
 
-const siteInitForm = ref<SiteInitRequest>({
-  siteTitle: "",
-  siteSubtitle: "",
-  siteDescription: "",
-  siteExternalUrl: "",
-  username: "",
-  password: "",
-  email: ""
+const siteInitFormSchema = yup.object({
+  siteTitle: yup.string().trim().required(t("siteInitView.form.verify.siteTitle.required")),
+  siteSubtitle: yup.string().trim().required(t("siteInitView.form.verify.siteSubtitle.required")),
+  siteDescription: yup.string().trim().required(t("siteInitView.form.verify.siteDescription.required")),
+  siteExternalUrl: yup
+    .string()
+    .trim()
+    .url(t("siteInitView.form.verify.siteExternalUrl.invalid"))
+    .required(t("siteInitView.form.verify.siteExternalUrl.required")),
+  username: yup
+    .string()
+    .trim()
+    .min(4, t("siteInitView.form.verify.username.min"))
+    .max(20, t("siteInitView.form.verify.username.min"))
+    .required(t("siteInitView.form.verify.username.required")),
+  password: yup
+    .string()
+    .trim()
+    .min(8, t("siteInitView.form.verify.password.min"))
+    .max(32, t("siteInitView.form.verify.password.min"))
+    .matches(
+      /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,32}$/,
+      t("siteInitView.form.verify.password.invalid")
+    )
+    .required(t("siteInitView.form.verify.password.required")),
+  email: yup
+    .string()
+    .trim()
+    .email(t("siteInitView.form.verify.email.invalid"))
+    .required(t("siteInitView.form.verify.email.required"))
 });
 
-function handleSubmit() {
-  const siteInitRequest = siteInitForm.value;
+const { handleSubmit } = useForm({
+  validationSchema: siteInitFormSchema
+});
+
+const onSubmit = handleSubmit((values) => {
+  const initRequest: SiteInitRequest = {
+    siteTitle: values.siteTitle,
+    siteSubtitle: values.siteSubtitle,
+    siteDescription: values.siteDescription,
+    siteExternalUrl: values.siteExternalUrl,
+    username: values.username,
+    password: values.password,
+    email: values.email
+  };
+  initSite(initRequest);
+});
+
+function initSite(siteInitRequest: SiteInitRequest) {
   commonApi
     .apiSiteInitPost({ siteInitRequest })
     .then((response) => {
       const resp = response.data;
       if (resp.isSuccessful) {
+        toast.add({ severity: "success", summary: t("siteInitView.toast.successTitle"), life: 3000 });
         router.push({ name: "login" });
+      } else {
+        toast.add({ severity: "warn", summary: t("siteInitView.toast.failedTitle"), detail: resp.message, life: 3000 });
       }
     })
     .catch((error) => {
@@ -40,171 +83,80 @@ function handleSubmit() {
 </script>
 
 <template>
-  <div class="mx-auto max-w-screen-xl px-4 py-16 sm:px-6 lg:px-8">
-    <div class="mx-auto max-w-lg text-center">
+  <div class="h-screen flex flex-col flex-grow items-center justify-center gap-2">
+    <div class="text-center">
       <h1 class="text-2xl font-bold sm:text-3xl">
-        {{ t("siteInitView.siteInitTitle") }}
+        {{ t("siteInitView.title") }}
       </h1>
 
       <p class="mt-4 text-gray-500">
-        {{ t("siteInitView.siteInitDesc") }}
+        {{ t("siteInitView.description") }}
       </p>
     </div>
 
-    <form action="#" @submit.prevent="handleSubmit" class="mx-auto mb-0 mt-8 max-w-md space-y-4">
-      <div>
-        <label for="siteTitle" class="sr-only">SiteTitle</label>
-
-        <div class="relative">
-          <input
-            id="siteTitle"
-            type="text"
-            class="w-full rounded-lg border-gray-200 p-4 pe-12 text-sm shadow-sm"
-            :placeholder="t('siteInitView.siteInitSiteTitlePlaceholder')"
-            pattern="\S+.*"
-            v-model="siteInitForm.siteTitle"
-            :title="t('siteInitView.siteInitSiteTitleRequirements')"
-            required
+    <form @submit="onSubmit" class="flex flex-col gap-4 m-4 w-96">
+      <div class="flex flex-col gap-2.5 w-full">
+        <div class="flex flex-col gap-1">
+          <VeeFloatInputText
+            id="initFormSiteTitle"
+            name="siteTitle"
+            :label="t('siteInitView.form.siteTitle')"
+            end-icon="mdi:card-text-outline"
           />
-
-          <span class="absolute inset-y-0 end-0 grid place-content-center px-4">
-            <Icon icon="mdi:card-text-outline" class="text-gray-400" />
-          </span>
         </div>
-      </div>
-
-      <div>
-        <label for="siteSubTitle" class="sr-only">SiteSubTitle</label>
-
-        <div class="relative">
-          <input
-            id="siteSubTitle"
-            type="text"
-            class="w-full rounded-lg border-gray-200 p-4 pe-12 text-sm shadow-sm"
-            :placeholder="t('siteInitView.siteInitSiteSubtitlePlaceholder')"
-            v-model="siteInitForm.siteSubtitle"
-            :title="t('siteInitView.siteInitSiteSubtitleRequirements')"
-            required
+        <div class="flex flex-col gap-1">
+          <VeeFloatInputText
+            id="initFormSiteSubtitle"
+            name="siteSubtitle"
+            :label="t('siteInitView.form.siteSubtitle')"
+            end-icon="mdi:subtitles-outline"
           />
-
-          <span class="absolute inset-y-0 end-0 grid place-content-center px-4">
-            <Icon icon="mdi:subtitles-outline" class="text-gray-400" />
-          </span>
         </div>
-      </div>
-
-      <div>
-        <label for="siteDescription" class="sr-only">SiteDescription</label>
-
-        <div class="relative">
-          <input
-            id="siteDescription"
-            type="text"
-            class="w-full rounded-lg border-gray-200 p-4 pe-12 text-sm shadow-sm"
-            :placeholder="t('siteInitView.siteInitSiteDescriptionPlaceholder')"
-            v-model="siteInitForm.siteDescription"
-            :title="t('siteInitView.siteInitSiteDescriptionRequirements')"
-            required
+        <div class="flex flex-col gap-1">
+          <VeeFloatInputText
+            id="initFormSiteDescription"
+            name="siteDescription"
+            :label="t('siteInitView.form.siteDescription')"
+            end-icon="mdi:file-document-box-outline"
           />
-
-          <span class="absolute inset-y-0 end-0 grid place-content-center px-4">
-            <Icon icon="mdi:file-document-box-outline" class="text-gray-400" />
-          </span>
         </div>
-      </div>
-
-      <div>
-        <label for="siteExternalUrl" class="sr-only">SiteExternalUrl</label>
-
-        <div class="relative">
-          <input
-            id="siteExternalUrl"
+        <div class="flex flex-col gap-1">
+          <VeeFloatInputText
+            id="initFormSiteExternalUrl"
+            name="siteExternalUrl"
+            :label="t('siteInitView.form.siteExternalUrl')"
             type="url"
-            class="w-full rounded-lg border-gray-200 p-4 pe-12 text-sm shadow-sm"
-            :placeholder="t('siteInitView.siteInitSiteExternalUrlPlaceholder')"
-            v-model="siteInitForm.siteExternalUrl"
-            :title="t('siteInitView.siteInitSiteExternalUrlRequirements')"
-            required
+            end-icon="mdi:link"
           />
-
-          <span class="absolute inset-y-0 end-0 grid place-content-center px-4">
-            <Icon icon="mdi:link" class="text-gray-400" />
-          </span>
         </div>
-      </div>
-
-      <div>
-        <label for="username" class="sr-only">Username</label>
-
-        <div class="relative">
-          <input
-            id="username"
-            type="text"
-            autocomplete="username"
-            class="w-full rounded-lg border-gray-200 p-4 pe-12 text-sm shadow-sm"
-            :placeholder="t('siteInitView.siteInitUsernamePlaceholder')"
-            v-model="siteInitForm.username"
-            pattern="^[a-zA-Z0-9]{4,20}$"
-            :title="t('siteInitView.siteInitUsernameRequirements')"
-            required
+        <div class="flex flex-col gap-1">
+          <VeeFloatInputText
+            id="initFormUsername"
+            name="username"
+            :label="t('siteInitView.form.username')"
+            end-icon="mdi:user"
           />
-
-          <span class="absolute inset-y-0 end-0 grid place-content-center px-4">
-            <Icon icon="mdi:user" class="text-gray-400" />
-          </span>
         </div>
-      </div>
-
-      <div>
-        <label for="password" class="sr-only">Password</label>
-
-        <div class="relative">
-          <input
-            id="password"
+        <div class="flex flex-col gap-1">
+          <VeeFloatInputText
+            id="initFormPassword"
+            name="password"
+            :label="t('siteInitView.form.password')"
             type="password"
-            autocomplete="new-password"
-            class="w-full rounded-lg border-gray-200 p-4 pe-12 text-sm shadow-sm"
-            :placeholder="t('siteInitView.siteInitPasswordPlaceholder')"
-            v-model="siteInitForm.password"
-            pattern="^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,32}$"
-            :title="t('siteInitView.siteInitPasswordRequirements')"
-            required
+            end-icon="mdi:password-outline"
           />
-
-          <span class="absolute inset-y-0 end-0 grid place-content-center px-4">
-            <Icon icon="mdi:password-outline" class="text-gray-400" />
-          </span>
         </div>
-      </div>
-
-      <div>
-        <label for="email" class="sr-only">Email</label>
-
-        <div class="relative">
-          <input
-            id="email"
+        <div class="flex flex-col gap-1">
+          <VeeFloatInputText
+            id="initFormEmail"
+            name="email"
+            :label="t('siteInitView.form.email')"
             type="email"
-            class="w-full rounded-lg border-gray-200 p-4 pe-12 text-sm shadow-sm"
-            :placeholder="t('siteInitView.siteInitEmailPlaceholder')"
-            v-model="siteInitForm.email"
-            :title="t('siteInitView.siteInitEmailRequirements')"
-            required
+            end-icon="mdi:alternate-email"
           />
-
-          <span class="absolute inset-y-0 end-0 grid place-content-center px-4">
-            <Icon icon="mdi:alternate-email" class="text-gray-400" />
-          </span>
         </div>
       </div>
-
-      <div class="flex items-center justify-center">
-        <button
-          type="submit"
-          class="w-1/2 inline-block rounded-lg bg-blue-500 px-5 py-3 text-sm font-medium text-white"
-        >
-          {{ t("siteInitView.siteInitSubmitButton") }}
-        </button>
-      </div>
+      <Button type="submit" :label="t('siteInitView.form.submitButton')" fluid />
     </form>
   </div>
 </template>
