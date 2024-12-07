7edf9428027091af7200c9ae512768c68580d0fb
Shiina Kin <shiina@sakurasou.io>
2024-12-08 00:27:00 +0800

    refactor(ui): refactor loginView

--- ui/src/views/auth/LoginView.vue
+++ ui/src/views/auth/LoginView.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
-import { ref } from "vue";
 import { useRouter } from "vue-router";
 import { useI18n } from "vue-i18n";
-import { useCommonStore } from "@/stores/counter";
-import { Icon } from "@iconify/vue";
-import { AuthApi, type UserLoginRequest } from "api-client";
 import { useToast } from "primevue/usetoast";
+import { useCommonStore } from "@/stores/counter";
+import { useForm } from "vee-validate";
+import * as yup from "yup";
+import { AuthApi, type UserLoginRequest } from "api-client";
+import VeeInputText from "@/components/vee-input/VeeInputText.vue";
+import Button from "primevue/button";
 
 const { t } = useI18n();
 const commonStore = useCommonStore();
@@ -14,23 +16,49 @@ const toast = useToast();
 
 const authApi = new AuthApi();
 
-const userLoginForm = ref<UserLoginRequest>({
-  password: "",
-  username: ""
+const loginFormSchema = yup.object({
+  username: yup
+    .string()
+    .trim()
+    .min(4, t("loginView.form.verify.username.min"))
+    .max(20, t("loginView.form.verify.username.min"))
+    .required(t("loginView.form.verify.username.required")),
+  password: yup
+    .string()
+    .trim()
+    .min(8, t("loginView.form.verify.password.min"))
+    .max(32, t("loginView.form.verify.password.min"))
+    .matches(
+      /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,32}$/,
+      t("loginView.form.verify.password.invalid")
+    )
+    .required(t("loginView.form.verify.password.required"))
 });
 
-function handleSubmit() {
-  const userLoginRequest = userLoginForm.value;
+const { handleSubmit } = useForm({
+  validationSchema: loginFormSchema
+});
+
+const onSubmit = handleSubmit((values) => {
+  const loginRequest: UserLoginRequest = {
+    username: values.username,
+    password: values.password
+  };
+  login(loginRequest);
+});
+
+function login(loginRequest: UserLoginRequest) {
   authApi
-    .apiUserLoginPost({ userLoginRequest })
+    .apiUserLoginPost({ userLoginRequest: loginRequest })
     .then((response) => {
       const resp = response.data;
       if (resp.isSuccessful) {
         const token = resp.data!.token;
         localStorage.setItem("token", token!);
+        toast.add({ severity: "success", summary: t("loginView.toast.successTitle"), life: 3000 });
         router.push({ name: "userField" });
       } else {
-        toast.add({ severity: "warn", summary: "Login Failed", detail: resp.message, life: 3000 });
+        toast.add({ severity: "warn", summary: t("loginView.toast.failedTitle"), detail: resp.message, life: 3000 });
       }
     })
     .catch((error) => {
@@ -41,70 +69,47 @@ function handleSubmit() {
 </script>
 
 <template>
-  <div class="h-screen w-full flex justify-center items-center">
-    <div class="mx-auto min-w-96">
-      <h1 class="text-center text-2xl font-bold text-indigo-600 sm:text-3xl">{{ commonStore.title }}</h1>
-
-      <form
-        @submit.prevent="handleSubmit"
-        class="mb-0 mt-6 space-y-4 rounded-lg p-4 shadow-lg sm:p-6 lg:p-8"
+  <div class="h-screen w-full flex justify-center items-center bg-gray-300">
+    <div class="relative w-1/2 rounded-xl shadow-lg overflow-hidden">
+      <img src="@/assets/authBackground.png" alt="authBackground" class="w-full object-cover" />
+      <div
+        class="absolute top-0 right-0 w-1/2 h-full bg-white bg-opacity-75 backdrop-blur-sm flex flex-col items-center justify-center gap-4"
       >
-        <p class="text-center text-lg font-medium">{{ t("loginView.loginTitle") }}</p>
+        <h2 class="text-3xl text-gray-700">{{ commonStore.title }}</h2>
 
-        <div>
-          <label for="username" class="sr-only">Username</label>
-
-          <div class="relative">
-            <input
-              id="username"
-              type="text"
-              autocomplete="username"
-              class="w-full rounded-lg border-gray-200 p-4 pe-12 text-sm shadow-sm"
-              :placeholder="t('loginView.loginUsernamePlaceholder')"
-              v-model="userLoginForm.username"
-              pattern="^[a-zA-Z0-9]{4,20}$"
-              :title="t('loginView.loginUsernameRequirements')"
-              required
+        <h4 class="text-xl text-gray-700">{{ t("loginView.title") }}</h4>
+        <form @submit="onSubmit" class="flex flex-col gap-4 w-96">
+          <div class="flex flex-col gap-1">
+            <VeeInputText
+              id="loginFormUsername"
+              name="username"
+              :label="t('loginView.form.username')"
+              start-icon="mdi:user"
             />
-
-            <span class="absolute inset-y-0 end-0 grid place-content-center px-4">
-              <Icon icon="mdi:user" class="text-gray-400" />
-            </span>
           </div>
-        </div>
-
-        <div>
-          <label for="password" class="sr-only">Password</label>
-
-          <div class="relative">
-            <input
-              id="password"
+          <div class="flex flex-col gap-1">
+            <VeeInputText
+              id="loginFormPassword"
+              name="password"
+              :label="t('loginView.form.password')"
               type="password"
-              autocomplete="current-password"
-              class="w-full rounded-lg border-gray-200 p-4 pe-12 text-sm shadow-sm"
-              :placeholder="t('loginView.loginPasswordPlaceholder')"
-              v-model="userLoginForm.password"
-              pattern="^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,32}$"
-              :title="t('loginView.loginPasswordRequirements')"
-              required
+              start-icon="mdi:password-outline"
             />
-
-            <span class="absolute inset-y-0 end-0 grid place-content-center px-4">
-              <Icon icon="mdi:password-outline" class="text-gray-400" />
-            </span>
           </div>
-        </div>
-
-        <button type="submit" class="block w-full rounded-lg bg-indigo-600 px-5 py-3 text-sm font-medium text-white">
-          {{ t("loginView.loginSubmitButton") }}
-        </button>
-
-        <p v-if="commonStore.allowSignup" class="text-center text-sm text-gray-500">
-          <a class="underline" href="#" @click="router.push({ name: 'register' })">{{
-            t("loginView.loginSignupButton")
-          }}</a>
-        </p>
-      </form>
+          <div class="flex flex-col gap-2">
+            <Button type="submit" :label="t('loginView.form.loginButton')" fluid />
+            <div class="flex justify-center">
+              <a
+                v-if="commonStore.allowSignup"
+                class="text-gray-500 hover:underline hover:text-gray-700 hover:cursor-pointer"
+                @click="router.push({ name: 'register' })"
+              >
+                {{ t("loginView.form.signupButton") }}
+              </a>
+            </div>
+          </div>
+        </form>
+      </div>
     </div>
   </div>
 </template>
