36a487533166d0d79dc71b49050aae418528a50d
Shiina Kin <shiina@sakurasou.io>
2024-12-08 00:41:15 +0800

    refactor(ui): refactor registerView

--- ui/src/views/auth/RegisterView.vue
+++ ui/src/views/auth/RegisterView.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
-import { ref } from "vue";
 import { useRouter } from "vue-router";
 import { useI18n } from "vue-i18n";
-import { useCommonStore } from "@/stores/counter";
-import { AuthApi, type UserInsertRequest } from "api-client";
-import { Icon } from "@iconify/vue";
 import { useToast } from "primevue/usetoast";
+import { useCommonStore } from "@/stores/counter";
+import { useForm } from "vee-validate";
+import * as yup from "yup";
+import { AuthApi, type UserInsertRequest } from "api-client";
+import VeeInputText from "@/components/vee-input/VeeInputText.vue";
+import Button from "primevue/button";
 
 const { t } = useI18n();
 const commonStore = useCommonStore();
@@ -14,22 +16,53 @@ const toast = useToast();
 
 const authApi = new AuthApi();
 
-const userRegisterForm = ref<UserInsertRequest>({
-  username: "",
-  password: "",
-  email: ""
+const signupFormSchema = yup.object({
+  username: yup
+    .string()
+    .trim()
+    .min(4, t("registerView.form.verify.username.min"))
+    .max(20, t("registerView.form.verify.username.min"))
+    .required(t("registerView.form.verify.username.required")),
+  password: yup
+    .string()
+    .trim()
+    .min(8, t("registerView.form.verify.password.min"))
+    .max(32, t("registerView.form.verify.password.min"))
+    .matches(
+      /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,32}$/,
+      t("registerView.form.verify.password.invalid")
+    )
+    .required(t("registerView.form.verify.password.required")),
+  email: yup
+    .string()
+    .trim()
+    .email(t("registerView.form.verify.email.invalid"))
+    .required(t("registerView.form.verify.email.required"))
 });
 
-function handleSubmit() {
-  const userRegisterRequest = userRegisterForm.value;
+const { handleSubmit } = useForm({
+  validationSchema: signupFormSchema
+});
+
+const onSubmit = handleSubmit((values) => {
+  const signupRequest: UserInsertRequest = {
+    username: values.username,
+    password: values.password,
+    email: values.email
+  };
+  signup(signupRequest);
+});
+
+function signup(signupRequest: UserInsertRequest) {
   authApi
-    .apiUserSignupPost({ userInsertRequest: userRegisterRequest })
+    .apiUserSignupPost({ userInsertRequest: signupRequest })
     .then((response) => {
       const resp = response.data;
       if (resp.isSuccessful) {
+        toast.add({ severity: "success", summary: t("registerView.toast.successTitle"), life: 3000 });
         router.push({ name: "login" });
       } else {
-        toast.add({ severity: "warn", summary: "Register Failed", detail: resp.message, life: 3000 });
+        toast.add({ severity: "warn", summary: t("registerView.toast.failedTitle"), detail: resp.message, life: 3000 });
       }
     })
     .catch((error) => {
@@ -40,90 +73,56 @@ function handleSubmit() {
 </script>
 
 <template>
-  <div class="h-screen w-full flex justify-center items-center">
-    <div class="mx-auto min-w-96">
-      <h1 class="text-center text-2xl font-bold text-indigo-600 sm:text-3xl">{{ commonStore.title }}</h1>
-
-      <form
-        @submit.prevent="handleSubmit"
-        class="mb-0 mt-6 space-y-4 rounded-lg p-4 shadow-lg sm:p-6 lg:p-8"
+  <div class="h-screen w-full flex justify-center items-center bg-gray-300">
+    <div class="relative w-1/2 rounded-xl shadow-lg overflow-hidden">
+      <img src="@/assets/authBackground.png" alt="authBackground" class="w-full object-cover" />
+      <div
+        class="absolute top-0 right-0 w-1/2 h-full bg-white bg-opacity-75 backdrop-blur-sm flex flex-col items-center justify-center gap-4"
       >
-        <p class="text-center text-lg font-medium">{{ t("registerView.registerTitle") }}</p>
+        <h2 class="text-3xl text-gray-700">{{ commonStore.title }}</h2>
 
-        <div>
-          <label for="username" class="sr-only">Username</label>
-
-          <div class="relative">
-            <input
-              id="username"
-              type="text"
-              autocomplete="username"
-              class="w-full rounded-lg border-gray-200 p-4 pe-12 text-sm shadow-sm"
-              :placeholder="t('registerView.registerUsernamePlaceholder')"
-              v-model="userRegisterForm.username"
-              pattern="^[a-zA-Z0-9]{4,20}$"
-              :title="t('registerView.registerUsernameRequirements')"
-              required
+        <h4 class="text-xl text-gray-700">{{ t("registerView.title") }}</h4>
+        <form @submit="onSubmit" class="flex flex-col gap-4 w-96">
+          <div class="flex flex-col gap-1">
+            <VeeInputText
+              id="signupFormUsername"
+              name="username"
+              :label="t('registerView.form.username')"
+              start-icon="mdi:user"
             />
-
-            <span class="absolute inset-y-0 end-0 grid place-content-center px-4">
-              <Icon icon="mdi:user" class="text-gray-400" />
-            </span>
           </div>
-        </div>
-
-        <div>
-          <label for="password" class="sr-only">Password</label>
-
-          <div class="relative">
-            <input
-              id="password"
+          <div class="flex flex-col gap-1">
+            <VeeInputText
+              id="signupFormPassword"
+              name="password"
+              :label="t('registerView.form.password')"
               type="password"
-              autocomplete="new-password"
-              class="w-full rounded-lg border-gray-200 p-4 pe-12 text-sm shadow-sm"
-              :placeholder="t('registerView.registerPasswordPlaceholder')"
-              v-model="userRegisterForm.password"
-              pattern="^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,32}$"
-              :title="t('registerView.registerPasswordRequirements')"
-              required
+              start-icon="mdi:password-outline"
             />
-
-            <span class="absolute inset-y-0 end-0 grid place-content-center px-4">
-              <Icon icon="mdi:password-outline" class="text-gray-400" />
-            </span>
           </div>
-        </div>
-
-        <div>
-          <label for="email" class="sr-only">Email</label>
-
-          <div class="relative">
-            <input
-              id="email"
+          <div class="flex flex-col gap-1">
+            <VeeInputText
+              id="signupFormEmail"
+              name="email"
+              :label="t('siteInitView.form.email')"
               type="email"
-              class="w-full rounded-lg border-gray-200 p-4 pe-12 text-sm shadow-sm"
-              :placeholder="t('registerView.registerEmailPlaceholder')"
-              v-model="userRegisterForm.email"
-              :title="t('registerView.registerEmailRequirements')"
-              required
+              start-icon="mdi:alternate-email"
             />
-
-            <span class="absolute inset-y-0 end-0 grid place-content-center px-4">
-              <Icon icon="mdi:alternate-email" class="text-gray-400" />
-            </span>
           </div>
-        </div>
-
-        <button type="submit" class="block w-full rounded-lg bg-indigo-600 px-5 py-3 text-sm font-medium text-white">
-          {{ t("registerView.registerSubmitButton") }}
-        </button>
-
-        <p class="text-center text-sm text-gray-500">
-          <a class="underline" href="#" @click="router.push({ name: 'login' })">{{
-            t("registerView.registerLoginButton")
-          }}</a>
-        </p>
-      </form>
+          <div class="flex flex-col gap-2">
+            <Button type="submit" :label="t('registerView.form.signupButton')" fluid />
+            <div class="flex justify-center">
+              <a
+                v-if="commonStore.allowSignup"
+                class="text-gray-500 hover:underline hover:text-gray-700 hover:cursor-pointer"
+                @click="router.push({ name: 'login' })"
+              >
+                {{ t("loginView.form.loginButton") }}
+              </a>
+            </div>
+          </div>
+        </form>
+      </div>
     </div>
   </div>
 </template>
