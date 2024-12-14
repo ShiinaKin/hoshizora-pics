e62e1378186f2ec4c00818502f3e4bfd9ae8db8f
Shiina Kin <shiina@sakurasou.io>
2024-12-15 01:44:19 +0800

    chore: add PNPM setup and build tasks to Gradle configuration

--- ui/build.gradle.kts
+++ ui/build.gradle.kts
@@ -1,3 +1,26 @@
+import com.github.gradle.node.pnpm.task.PnpmTask
+
+plugins {
+    id("com.github.node-gradle.node") version "7.1.0"
+}
+
+node {
+    version = "20.18.1"
+    workDir = file("${projectDir}/node")
+    download = true
+    pnpmVersion = "9.15.0"
+}
+
+tasks.register<PnpmTask>("build-packages") {
+    dependsOn("pnpmInstall")
+    args = listOf("build-packages")
+}
+
+tasks.register<PnpmTask>("build") {
+    dependsOn("build-packages")
+    args = listOf("build")
+}
+
 tasks.register<Delete>("cleanDist") {
     delete("${projectDir}\\dist")
 }
