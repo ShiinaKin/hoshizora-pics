ed9f5cda3a31a25f3b0aa95d28aafadd3fd90dfd
Shiina Kin <shiina@sakurasou.io>
2024-12-18 23:04:16 +0800

    refactor: simplify logic in S3Utils

--- app/src/main/kotlin/io/sakurasou/util/S3Utils.kt
+++ app/src/main/kotlin/io/sakurasou/util/S3Utils.kt
@@ -3,18 +3,14 @@ package io.sakurasou.util
 import io.ktor.util.*
 import io.sakurasou.model.entity.Strategy
 import io.sakurasou.model.strategy.S3Strategy
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
 import org.apache.commons.codec.digest.DigestUtils
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider
-import software.amazon.awssdk.core.exception.SdkClientException
 import software.amazon.awssdk.core.sync.RequestBody
 import software.amazon.awssdk.regions.Region
 import software.amazon.awssdk.services.s3.S3Client
-import software.amazon.awssdk.services.s3.model.DeleteObjectRequest
-import software.amazon.awssdk.services.s3.model.PutObjectRequest
-import software.amazon.awssdk.services.s3.model.S3Exception
+import software.amazon.awssdk.services.s3.model.*
+import java.io.InputStream
 import java.net.URI
 
 /**
@@ -24,54 +20,38 @@ import java.net.URI
 object S3Utils {
     private val s3ClientMap by lazy { mutableMapOf<Long, S3Client>() }
 
-    suspend fun uploadImage(uploadPath: String, imageBytes: ByteArray, strategy: Strategy) {
-        withContext(Dispatchers.IO) {
+    fun uploadImage(uploadPath: String, imageBytes: ByteArray, strategy: Strategy) {
+        runCatching {
             val strategyConfig = strategy.config as S3Strategy
-            runCatching {
-                getOrCreateS3Client(strategy, strategyConfig)
-            }.onFailure {
-                throw RuntimeException("Failed to create S3 client", it)
-            }.onSuccess { s3Client ->
-                val sha256 = DigestUtils.sha256(imageBytes).encodeBase64()
-                val putObject = PutObjectRequest.builder()
-                    .bucket(strategyConfig.bucketName)
-                    .key(uploadPath)
-                    .checksumSHA256(sha256)
-                    .build()
-                runCatching {
-                    s3Client.putObject(putObject, RequestBody.fromBytes(imageBytes))
-                }.onFailure {
-                    when (it) {
-                        is SdkClientException -> throw RuntimeException("Failed to upload image to S3", it)
-                        is S3Exception -> throw RuntimeException("Failed to upload image to S3", it)
-                        else -> throw it
-                    }
-                }
+            val s3Client = getOrCreateS3Client(strategy, strategyConfig)
+            val sha256 = DigestUtils.sha256(imageBytes).encodeBase64()
+            val putObject = PutObjectRequest.builder()
+                .bucket(strategyConfig.bucketName)
+                .key(uploadPath)
+                .checksumSHA256(sha256)
+                .build()
+            s3Client.putObject(putObject, RequestBody.fromBytes(imageBytes))
+        }.onFailure {
+            when (it) {
+                is S3Exception -> throw RuntimeException("Failed to upload image to S3", it)
+                else -> throw it
             }
         }
     }
 
-    suspend fun deleteImage(relativePath: String, strategy: Strategy) {
-        withContext(Dispatchers.IO) {
+    fun deleteImage(relativePath: String, strategy: Strategy) {
+        runCatching {
             val strategyConfig = strategy.config as S3Strategy
-            runCatching {
-                getOrCreateS3Client(strategy, strategyConfig)
-            }.onFailure {
-                throw RuntimeException("Failed to create S3 client", it)
-            }.onSuccess { s3Client ->
-                val deleteObjectRequest = DeleteObjectRequest.builder()
-                    .bucket(strategyConfig.bucketName)
-                    .key(relativePath)
-                    .build()
-                runCatching {
-                    s3Client.deleteObject(deleteObjectRequest)
-                }.onFailure {
-                    when (it) {
-                        is SdkClientException -> throw RuntimeException("Failed to delete image from S3", it)
-                        is S3Exception -> throw RuntimeException("Failed to delete image from S3", it)
-                        else -> throw it
-                    }
-                }
+            val s3Client = getOrCreateS3Client(strategy, strategyConfig)
+            val deleteObjectRequest = DeleteObjectRequest.builder()
+                .bucket(strategyConfig.bucketName)
+                .key(relativePath)
+                .build()
+            s3Client.deleteObject(deleteObjectRequest)
+        }.onFailure {
+            when (it) {
+                is S3Exception -> throw RuntimeException("Failed to delete image from S3", it)
+                else -> throw it
             }
         }
     }
@@ -79,21 +59,18 @@ object S3Utils {
     private fun getOrCreateS3Client(
         strategy: Strategy,
         strategyConfig: S3Strategy
-    ): S3Client = s3ClientMap[strategy.id] ?: run {
-        val s3 = S3Client.builder()
-            .region(Region.of(strategyConfig.region))
-            .endpointOverride(URI(strategyConfig.endpoint))
-            .credentialsProvider(
-                StaticCredentialsProvider.create(
-                    AwsBasicCredentials.create(
-                        strategyConfig.accessKey,
-                        strategyConfig.secretKey
-                    )
+    ): S3Client = s3ClientMap[strategy.id] ?: S3Client.builder()
+        .region(Region.of(strategyConfig.region))
+        .endpointOverride(URI(strategyConfig.endpoint))
+        .credentialsProvider(
+            StaticCredentialsProvider.create(
+                AwsBasicCredentials.create(
+                    strategyConfig.accessKey,
+                    strategyConfig.secretKey
                 )
             )
-            .forcePathStyle(true)
-            .build()
-        s3ClientMap[strategy.id] = s3
-        s3
-    }
+        )
+        .forcePathStyle(true)
+        .build()
+        .also { s3ClientMap[strategy.id] = it }
 }
