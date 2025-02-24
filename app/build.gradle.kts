40faf215bc9dd46b3de329e4a91fe1674d9381b8
Shiina Kin <shiina@sakurasou.io>
2025-02-24 15:38:18 +0800

    chore: bump dependecy version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,7 +32,7 @@ val mockkVersion: String by project
 plugins {
     kotlin("jvm") version "2.1.10"
     kotlin("plugin.serialization") version "2.1.10"
-    id("io.ktor.plugin") version "3.0.3"
+    id("io.ktor.plugin") version "3.1.0"
 }
 
 application {
