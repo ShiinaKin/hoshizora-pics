34008947d275c4bcfbb147ed4bbcdd9d30f51390
Shiina Kin <shiina@sakurasou.io>
2025-02-24 13:23:44 +0800

    feat: add WebDavException for WebDav ops

---
+++ app/src/main/kotlin/io/sakurasou/exception/service/image/io/webdav/WebDavException.kt
@@ -0,0 +1,19 @@
+package io.sakurasou.exception.service.image.io.webdav
+
+import io.sakurasou.exception.ServiceThrowable
+
+/**
+ * @author Shiina Kin
+ * 2025/2/24 13:07
+ */
+class WebDavException(
+    reason: String? = null
+) : ServiceThrowable() {
+    override val code: Int
+        get() = 5000
+    override var message: String = "WebDav Ops Failed"
+
+    init {
+        message += if (reason != null) ", $reason" else ""
+    }
+}
