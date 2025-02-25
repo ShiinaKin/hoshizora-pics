4d1c5f70ead72567728477d2be311a23166df89f
Shiina Kin <shiina@sakurasou.io>
2025-02-25 22:53:36 +0800

    chore: bump ktor version to 3.1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,7 +32,7 @@ val mockkVersion: String by project
 plugins {
     kotlin("jvm") version "2.1.10"
     kotlin("plugin.serialization") version "2.1.10"
-    id("io.ktor.plugin") version "3.1.0"
+    id("io.ktor.plugin") version "3.1.1"
 }
 
 application {
